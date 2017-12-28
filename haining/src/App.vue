387f5722b85b0d0409587d1a72c7dad296568d7b
zhangping <zhangping@dianhun.cn>
2017-12-28 21:23:23 +0800

    添加菜单及logo

--- haining/src/App.vue
+++ haining/src/App.vue
@@ -1,23 +1,124 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <!-- 头部导航 -->
+    <header class="header" :class="{ 'header-fixed' : headerFixed }">
+    <el-row>
+        <el-col :span="6">
+        <img src="./assets/logo.png" style="padding:5px 5px 5px 100px" />
+        </el-col>
+        <el-col :span="12">
+
+          <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+  <el-submenu index="1">
+    <template slot="title">系统管理</template>
+    <el-menu-item index="1-1">选项1</el-menu-item>
+    <el-menu-item index="1-2">选项2</el-menu-item>
+    <el-menu-item index="1-3">选项3</el-menu-item>
+  </el-submenu>
+  <el-submenu index="2">
+    <template slot="title">数据登记</template>
+          <el-menu-item index="2-1">选项1</el-menu-item>
+    <el-menu-item index="2-2">选项2</el-menu-item>
+    <el-menu-item index="2-3">选项3</el-menu-item>
+  </el-submenu>
+  <el-submenu index="3">
+    <template slot="title">数据登记</template>
+    <el-menu-item index="3-1">选项1</el-menu-item>
+    <el-menu-item index="3-2">选项2</el-menu-item>
+    <el-menu-item index="3-3">选项3</el-menu-item>
+  </el-submenu>
+</el-menu>
+
+        </el-col>
+
+    </el-row>
+    </header>
+    <div v-show="headerFixed" style="position: relative;height: 60px;width: 100%;"></div>
+
   </div>
 </template>
 
 <script>
+
+import Vue from 'vue'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import $ from 'jquery'
+
+Vue.use(Element)
+
 export default {
-  name: 'app'
+  name: 'app',
+  data: function (){
+    return {
+      active:true,
+      headerFixed : true
+    }
+  },
+  created: function(){
+    this.$router.push('/activePublic');
+  },
+  methods: {
+
+  },
+  watch: {
+     '$route': function (to,from) {
+         if(to.path == '/activePublic'){
+             this.active = true ;
+         }else if(to.path == '/activeManage'){
+             this.active = false ;
+         }
+     }
+  }
 }
 </script>
 
 <style>
+
+body{margin: 0;}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  min-width: 1200px;
+  margin: 0 auto;
+  font-family: "Helvetica Neue","PingFang SC",Arial,sans-serif;
 }
+/* 头部导航 */
+header{z-index: 1000;min-width: 1200px;transition: all 0.5s ease;  border-top: solid 4px #3091F2;  background-color: #fff;  box-shadow: 0 2px 4px 0 rgba(0,0,0,.12),0 0 6px 0 rgba(0,0,0,.04);  }
+header.header-fixed{position: fixed;top: 0;left: 0;right: 0;}
+header .el-menu-demo{padding-left: 100px!important;}
+
+/* 主内容区 */
+main{display: -webkit-box;display: -ms-flexbox;display: flex;  min-height: 800px;  border: solid 40px #E9ECF1;  background-color: #FCFCFC;  }
+main .main-left{text-align: center;-webkit-box-flex: 0;-ms-flex: 0 0 200px;flex: 0 0 200px;}
+main .main-right{-webkit-box-flex:1;-ms-flex:1;flex:1; background-color: #fff; padding: 50px 70px; }
+main .el-menu{background-color: transparent!important;}
+/*  */
+.router-link{display:inline-block;width:100%;height:100%;text-align:center;color:#475669;text-decoration: none; }
+.is-active .router-link{color:#20a0ff; }
+
+/* 单选框 */
+  .el-form-item .el-radio+.el-radio {
+    margin-left: 30px!important;
+  }
+  /* 路由切换动效 */
+  .fade-enter-active, .fade-leave-active {
+    transition: all .5s;
+  }
+  .fade-enter, .fade-leave-active {
+    opacity: 0;
+  }
+
+  .list-enter-active, .list-leave-active {
+    transition: all 1s;
+  }
+  .list-enter, .list-leave-active {
+    opacity: 0;
+    transform: translateY(30px);
+  }
+
+/* 导航栏菜单选中效果 */
+  .isActive{color: #20a0ff!important;}
+   #app main .aside .is-active{color: #475669;}
+
+  /* 卡片 */
+  .el-card{overflow: visible!important;}
 </style>
