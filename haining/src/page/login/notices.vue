298f198dfba44f7e74517215edd50a6871f73710
champion <376930948@qq.com>
2018-01-19 03:22:10 +0800

    文件名修改

--- haining/src/page/login/notices.vue
+++ haining/src/page/login/notices.vue
@@ -1,5 +1,7 @@
 <template>
-	
+	<div class="notices">
+	<span style="color:#C9A44E;font-size:20px;">系统通知</span>
+	</div>
 </template>
 <script>
 	export default{
@@ -7,6 +9,20 @@
 			return {
 
 			}
+		},
+		created:function(){
+			this.$http.post('/notice/getNoticeList',
+		         this.sq.stringify({
+		            'token':this.$store.state.token
+		         })
+		         )
+		         .then(function(res){
+
+		         })
+		         .catch(function(err){
+
+		         })
+			
 		},
 		methods:{
 			getMyNoticeCount(){
@@ -22,22 +38,20 @@
 
 		         })
 			},
-			getNoticeList(){
-				 this.$http.post('/notice/getNoticeList',
-		         this.sq.stringify({
-		            'token':this.$store.state.token
-		         })
-		         )
-		         .then(function(res){
-
-		         })
-		         .catch(function(err){
-
-		         })
-			}
+			
 		}
 	}
 </script>
 <style>
-	
+	.notices{
+		background-color:#FFFFFF;
+	    margin-top: 50px;
+	    margin-right:20%;
+	    margin-bottom: 100px;
+	    margin-left: 20%;
+	    padding-left: 240px;
+	    padding-top: 40px;
+	    padding-bottom: 40px;
+	}
+	    
 </style>
