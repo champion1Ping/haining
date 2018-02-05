8fd43c5874a50676bc48d71800de1b3e988c47c5
zhangping <zhangping@dianhun.cn>
2018-02-05 20:10:04 +0800

    角色菜单显示修复

--- haining/src/page/common/header.vue
+++ haining/src/page/common/header.vue
@@ -8,28 +8,28 @@
         <el-col :span="14">
         <div v-if="this.$store.state.roleId > 0 || id > 0 ">
           <el-menu  class="el-menu-demo" mode="horizontal" @select="handleSelect">
-			  <el-submenu index="1" v-if="this.$store.state.roleId==3 ||id ==3">
+			  <el-submenu index="1" v-if="this.$store.state.roleId==3 ">
 			    <template slot="title"><span style="font-size:20px;color:#333333;">系统管理</span></template>
 			    <el-menu-item index="/systemNotice">系统公告发布</el-menu-item>
 			    <el-menu-item index="/agencymanager">代理商创建</el-menu-item>
 			    <el-menu-item index="/usermanager">系统用户管理</el-menu-item>
 			    <el-menu-item index="/productgain">产品收益率维护</el-menu-item>
 			  </el-submenu>
-			  <el-submenu index="2" v-if="this.$store.state.roleId !==1 || id !==1">
+			  <el-submenu index="2" v-if="this.$store.state.roleId !=1 ">
 			    <template slot="title"><span style="font-size:20px;color:#333333">数据登记</span></template>
-			          <el-menu-item index="/customer" v-if="this.$store.state.roleId !==1 || id !==1">客户入金登记表</el-menu-item>
-			    <el-menu-item index="/contractgain" v-if="this.$store.state.roleId==3 ||id ==3">合同收益分配表</el-menu-item>
+			          <el-menu-item index="/customer" v-if="this.$store.state.roleId !=1">客户入金登记表</el-menu-item>
+			    <el-menu-item index="/contractgain" v-if="this.$store.state.roleId==3" >合同收益分配表</el-menu-item>
 			  </el-submenu>
 			  <el-submenu index="3">
 			    <template slot="title"><span style="font-size:20px;color:#333333">收益查询</span></template>
-			    <el-menu-item index="/customerGain" style="text-align:center" v-if="this.$store.state.roleId !==1 || id !==1">客户收益表</el-menu-item>
-			    <el-menu-item index="/agencyprofit" v-if="this.$store.state.roleId!==2 ">代理商收益表</el-menu-item>
-			    <el-menu-item index="/customerRecommend" v-if="this.$store.state.roleId==3 || id ==3">推荐客户表</el-menu-item>
+			    <el-menu-item index="/customerGain" style="text-align:center" v-if="this.$store.state.roleId !=1">客户收益表</el-menu-item>
+			    <el-menu-item index="/agencyprofit" v-if="this.$store.state.roleId!=2">代理商收益表</el-menu-item>
+			    <el-menu-item index="/customerRecommend" v-if="this.$store.state.roleId==3">推荐客户表</el-menu-item>
 			  </el-submenu>
 		  </el-menu>
 		  </div>
         </el-col>
-        <el-col :span="2" v-if="this.$store.state.roleId > 0||id > 0">
+        <el-col :span="2" v-if="this.$store.state.roleId > 0">
         	
         	<el-menu  class="el-menu-demo" mode="horizontal" @select="handleSelect">
 			  <el-submenu index="1">
@@ -37,15 +37,15 @@
 			    <template slot="title" ><span style="font-size:14px;color:#333333">{{this.$store.state.realName?this.$store.state.realName:userName}}</span></template>
 
 
-			    <el-menu-item index="/personinfo" v-if="this.$store.state.roleId==2 || id == 2">个人信息</el-menu-item>
-			    <el-menu-item index="/updatepw" v-if="this.$store.state.roleId  !=2 || id !=2">修改密码</el-menu-item>
+			    <el-menu-item index="/personinfo" v-if="this.$store.state.roleId==2 ">个人信息</el-menu-item>
+			    <el-menu-item index="/updatepw" v-if="this.$store.state.roleId  !=2 ">修改密码</el-menu-item>
 			    <el-menu-item index="/login" @click="loginout()">退出登录</el-menu-item>
 			  </el-submenu>
 			  
 		  </el-menu>
         </el-col>
 
-       	<el-col :span="2" v-if="this.$store.state.roleId > 0 || id > 0 ">
+       	<el-col :span="2" v-if="this.$store.state.roleId > 0">
        	<el-button type="text" @click="getNotice()" style="padding-top:8px;"><span style="line-height:3;font-size:14px;color:#FF6240;">通知&nbsp;&nbsp;&nbsp; (<span>{{this.$store.state.noticeCount?this.$store.state.noticeCount:count}}</span><span>)</span></span></el-button>
 
        	</el-col>
@@ -67,12 +67,13 @@
 		watch:{
 			userName(val,oldVal){
 				
-				alert(val+","+oldVal);
+				//alert(val+","+oldVal);
 			}
 		},
 		created:function(){
-			
+			alert("header"); 
 			this.id = sessionStorage.getItem("roleId");
+			this.$store.commit('saveRoleId', this.id);
 			console.log(this.roleId+","+this.id);
 			this.userName = sessionStorage.getItem("userName");
 			let me = this;
